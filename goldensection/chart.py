05973ab8b465c039304c31fd2d7648baef20db1e
stsdc <stanislaw.dac@gmail.com>
2018-10-27 12:08:43 +0200

    add labels and title

--- goldensection/chart.py
+++ goldensection/chart.py
@@ -5,15 +5,15 @@ from gi.repository import Gtk, Gdk, Granite, GdkPixbuf
 
 class Chart:
     def __init__(self):
-        fig = Figure(figsize=(5, 4), dpi=100)
-        a = fig.add_subplot(111)
+        figure = Figure()
+        subplot = figure.add_subplot(111, ylabel="f(x)", xlabel="x", title="Some func")
         t = np.arange(0.0, 3.0, 0.01)
         s = np.sin(2*np.pi*t)
 
-        a.plot(t, s)
+        subplot.plot(t, s)
 
         self.sw = Gtk.ScrolledWindow()
         # A scrolled window border goes outside the scrollbars and viewport
-        canvas = FigureCanvas(fig)  # a Gtk.DrawingArea
+        canvas = FigureCanvas(figure)  # a Gtk.DrawingArea
         # canvas.set_size_request(800, 600)
         self.sw.add_with_viewport(canvas)
